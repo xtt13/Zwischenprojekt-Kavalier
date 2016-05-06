2cb612cf08ca20d501a8937ba904dde2a8239b4b
xtt13 <Michi@Spina-Macbook.local>
2016-05-06 18:42:33 +0200

    Bug fix. header Ausgaben müssen über jeder Form von Ausgabe stehen!

--- views/summary.php
+++ views/summary.php
@@ -13,6 +13,50 @@
 
   //print_r($_POST);
 
+
+  // Wenn der Buy!-Button gedrückt wurde
+  if(isset($_POST['button-sbm-buy'])){
+
+    $created_at = time();
+    $user_id = $user[0]['id'];
+
+    // Die Bestellung wird in 'orders' eingetragen
+    $sql = "INSERT INTO orders (user_id, total_price) VALUES ('$user_id', '$gesamtpreis')";
+    mysqli_query($link, $sql) or die(mysqli_error($link));
+    // ID vom letzen Query wird ausgelesen
+    $order_id = mysqli_insert_id($link);
+
+    $bag = $_SESSION['bag'];
+
+    // Bag wird nochmal durchlaufen und der jeweilige Preis des Produkts wird erfasst
+    foreach($bag as $bag_keys){
+
+      $product_id = $bag_keys['id'];
+
+      // Query für Produkt
+      $sql = "SELECT * FROM products WHERE id = '$product_id'";
+      $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+      $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+      $quantity = $bag_keys['quantity'];
+      $price = $products['0']['price'];
+
+      // Einzelnen Produkte werden in 'producs_sold' geschrieben. Mit der order_id kann jede Bestellung wieder zusammengebaut werden.
+      $sql = "INSERT INTO products_sold (order_id, product_id, quantity, price) VALUES ('$order_id', '$product_id', '$quantity', '$price')";
+      $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+
+    }
+    // Der Abschnitt Summary ist OK (Ist  Berechtigung für nächsten Checkoutteil)
+    $_SESSION['summary'] = true;
+
+    // Warenkorb wird geleert
+    unset($_SESSION['bag']);
+
+    // Redirect zur Successseite
+    redirect_to("index.php?site=checkout&action=success");
+    echo "TEST";
+  }
+
  ?>
 
   <div class="wrapper-page shipping">
@@ -168,49 +212,3 @@
 
 
   </div>
-
-  <?php
-  // Wenn der Buy!-Button gedrückt wurde
-  if(isset($_POST['button-sbm-buy'])){
-
-    $created_at = time();
-    $user_id = $user[0]['id'];
-
-    // Die Bestellung wird in 'orders' eingetragen
-    $sql = "INSERT INTO orders (user_id, total_price) VALUES ('$user_id', '$gesamtpreis')";
-    mysqli_query($link, $sql) or die(mysqli_error($link));
-    // ID vom letzen Query wird ausgelesen
-    $order_id = mysqli_insert_id($link);
-
-    $bag = $_SESSION['bag'];
-
-    // Bag wird nochmal durchlaufen und der jeweilige Preis des Produkts wird erfasst
-    foreach($bag as $bag_keys){
-
-      $product_id = $bag_keys['id'];
-
-      // Query für Produkt
-      $sql = "SELECT * FROM products WHERE id = '$product_id'";
-      $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-      $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-      $quantity = $bag_keys['quantity'];
-      $price = $products['0']['price'];
-
-      // Einzelnen Produkte werden in 'producs_sold' geschrieben. Mit der order_id kann jede Bestellung wieder zusammengebaut werden.
-      $sql = "INSERT INTO products_sold (order_id, product_id, quantity, price) VALUES ('$order_id', '$product_id', '$quantity', '$price')";
-      $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-
-    }
-    // Der Abschnitt Summary ist OK (Ist  Berechtigung für nächsten Checkoutteil)
-    $_SESSION['summary'] = true;
-
-    // Warenkorb wird geleert
-    unset($_SESSION['bag']);
-
-    // Redirect zur Successseite
-    redirect_to("index.php?site=checkout&action=success");
-    echo "TEST";
-  }
-
-   ?>
