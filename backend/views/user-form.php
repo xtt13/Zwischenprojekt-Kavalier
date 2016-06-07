7946a2a039ff2e4e5ca27cca4062a92e544cf6f7
Hannah Schott <hannah.schott@hotmail.com>
2016-06-07 13:41:54 +0200

    user-form style fertig

--- backend/views/user-form.php
+++ backend/views/user-form.php
@@ -1,82 +1,98 @@
-<!-- <form>
 
-
-
-  fullname
-  email
-  password
-  street and number
-  zip and location
-  country
-  is_admin -->
 
   <div class="body-wrapper">
 
-    <h3 class="headline">New Product</h3>
+    <h3 class="headline">New User</h3>
 
-  <section class="product-form">
+  <section class="form-new">
 
 
     <form class="" action="index.html" method="post">
       <fieldset>
 
-        <div class="user-form">
+        <div class="form-wrapper">
           <h2 class="form-headline">Fullname</h2>
           <label for="fullname"></label>
-          <input type="text" name="fullname" class="user-name" value="">
+          <input type="text" name="fullname" class="form-input" value="">
         </div>
 
-        <div class="user-form">
+        <div class="form-wrapper">
           <h2 class="form-headline">Email</h2>
           <label for="email"></label>
-          <input type="text" name="email" class="user-email" value="">
+          <input type="text" name="email" class="form-input" value="">
         </div>
 
-        <div class="user-form">
+        <div class="form-wrapper">
           <h2 class="form-headline">Password</h2>
           <label for="password"></label>
-          <input type="password" name="password" class="user-password" value="">
-          <h3>Confirm Password</h3>
+          <input type="password" name="password" class="form-input" value="">
+        </div>
+        <div class="form-wrapper">
+          <h3 class="form-subheadline-diffrent">Confirm Password</h3>
           <label for="password-confirm"></label>
-          <input type="password-confirm" name="password-confirm" class="user-password" value="">
+          <input type="password" name="password-confirm" class="form-input" value="">
         </div>
 
-        <div class="user-form">
-          <h2 class="form-headline">Adress</h2>
 
-          <h3>Street</h3>
 
-          <label for="street"></label>
-          <input type="text" name="street" class="user-adress" value="">
+        <div class="form-wrapper">
+              <h2 class="form-headline">Adress</h2>
 
-          <h3>Number</h3>
+                <div class="form-element-wrapper">
 
-          <label for="number"></label>
-          <input type="text" name="number" class="user-adress" value="">
+                  <div class="form-wrapper">
+                  <h3 class="form-subheadline">Street</h3>
 
-          <h3>ZIP</h3>
+                  <label for="street"></label>
+                  <input type="text" name="street" class="input-street form-input" value="">
+                </div>
 
-          <label for="zip"></label>
-          <input type="text" name="zip" class="user-zip" value="">
+                <div class="form-wrapper">
 
-          <h3>Location</h3>
+                  <h3 class="form-subheadline">Number</h3>
 
-          <label for="location"></label>
-          <input type="text" name="location" class="user-adress" value="">
+                  <label for="number"></label>
+                  <input type="text" name="number" class="input-number form-input" value="">
+                </div>
 
-          <h3>Country</h3>
-          <label for="country"></label>
-          <input type="text" name="country" class="user-adress" value="">
-        </div>
+              </div>
 
-        <div class="user-form">
+              <div class="form-element-wrapper">
+                <div class="form-wraper">
+                  <h3 class="form-subheadline">ZIP</h3>
+
+                    <label for="zip"></label>
+                    <input type="text" name="zip" class="input-zip form-input" value="">
+                </div>
+
+
+                <div class="form-wrapper">
+                  <h3 class="form-subheadline">Location</h3>
+
+                  <label for="location"></label>
+                  <input type="text" name="location" class="input-location form-input" value="">
+                </div>
+
+                <div class="form-wrapper">
+                  <h3 class="form-subheadline">Country</h3>
+                  <label for="country"></label>
+                  <input type="text" name="country" class="input-country form-input" value="">
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+        <div class="checkbox-wrapper">
           <label for="admin">Administrator</label>
           <input type="checkbox" name="admin" value="Admin">
         </div>
 
 
 
-        <div class="product-form">
+        <div class="form-wrapper">
           <input type="submit"  class="save-button" value="Save">
         </div>
       </fieldset>
