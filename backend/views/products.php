f66cd9ffb6fa7f48d1b176dad96786e13268f838
xtt13 <Michi@Spina-Macbook.local>
2016-07-29 15:38:13 +0200

    Kleiner Fix

--- backend/views/products.php
+++ backend/views/products.php
@@ -20,7 +20,7 @@
       <?php $image_main = $product["image_main"]; ?>
       <tr class="space"></tr>
       <tr class="table_row">
-        <td> <?php echo "  <a class='image-link'><img src='../images/$image_main' alt='$image_main'></a>"; ?></td>
+        <td> <?php echo "  <a href='../index.php?site=detail&id=". $product['id'] . "' class='image-link'><img src='../images/$image_main' alt='$image_main'></a>"; ?></td>
         <td><?php echo $product['product_name']; ?></td>
         <td><?php echo $product['product_category']; ?></td>
         <td><?php echo $product['price']?>€</td>
